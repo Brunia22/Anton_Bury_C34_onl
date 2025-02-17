430fdaa423905b2e902672290898c024c6764534
Brunia22 <anton.buryy.97@mail.ru>
2025-02-17 22:41:55 +0300

    lesson_2

---
+++ lesson2/src/main/java/HW2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class HW2 {
+
+    public static void main(String[] args){
+
+        System.out.println("Enter number");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number1 =  scanner.nextInt();
+        int value1 =0;
+
+        while(number1 % 10 == 0){
+
+            number1 = number1 / 10;
+
+            value1= value1 + 1;
+        }
+        System.out.println("Number of zeros:" + value1);
+
+
+    }
+}
+
